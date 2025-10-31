28d953cba4db0a4f2c8f863058a4a95a0db6d8b2
Phu Pham <phu.pham@my.utsa.edu>
2025-10-31 02:36:22 -0500

    initial update

---
+++ BiteWise/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module utsa.cs3443.group7.bitewise {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens utsa.cs3443.group7.bitewise to javafx.fxml;
+    exports utsa.cs3443.group7.bitewise;
+}

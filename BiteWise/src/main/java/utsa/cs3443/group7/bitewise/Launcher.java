3632f519df666eeb4002344e40a103395f646693
Phu Pham <phu.pham@my.utsa.edu>
2025-10-31 03:16:56 -0500

    fresh start

--- BiteWise/src/main/java/utsa/cs3443/group7/bitewise/Launcher.java
+++
@@ -1,9 +0,0 @@
-package utsa.cs3443.group7.bitewise;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}

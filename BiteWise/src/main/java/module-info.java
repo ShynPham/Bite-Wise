e22f507ff6bcee51f3427417d6f5a07415d1d335
Phu Pham <phu.pham@my.utsa.edu>
2025-10-31 02:24:23 -0500

    deleted everything

--- BiteWise/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module edu.utsa.cs3443.bitewise {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens edu.utsa.cs3443.bitewise to javafx.fxml;
-    exports edu.utsa.cs3443.bitewise;
-}

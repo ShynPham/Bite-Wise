1bf7a2734639b84aaeb0b020dda77e8ac366bc2d
Phu Pham <phu.pham@my.utsa.edu>
2025-11-04 10:33:48 -0600

    add signUp screen

--- BiteWise/src/main/java/controller/entryScreen.java
+++ BiteWise/src/main/java/controller/entryScreen.java
@@ -7,7 +7,18 @@ import utility.viewSwitcher;
 public class entryScreen {
 
     @FXML
+    // Handle button when the SignIn button is click
     private void handleSignInClick(){
+        viewSwitcher.switchScene("sign-in.fxml");
+    }
+    @FXML
+    // Handle button when the SignUp button is click
+    private void handleSignUpClick(){
+        viewSwitcher.switchScene("sign-up.fxml");
+    }
+    @FXML
+    // Handle button click after either signing in or up
+    private void handleContinueButtonClick(){
         viewSwitcher.switchScene("scan-screen.fxml");
     }
 

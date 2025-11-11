12c0ca117c1b2fb75aafba1b41e73de8b6ac5706
AsianBMO <nguyen.angel@aol.com>
2025-11-11 12:11:45 -0600

    I deleted     requires edu.utsa.cs.group.bitewise;

--- BiteWise/src/main/java/controller/SettingsController.java
+++ BiteWise/src/main/java/controller/SettingsController.java
@@ -43,6 +43,8 @@ public  class SettingsController {
     private void handleScanScreenClick(){viewSwitcher.switchScene("scan-screen.fxml");}
 
     @FXML
+    private void handleBackToSettings() { viewSwitcher.switchScene("settings-screen.fxml");}
+    @FXML
     private void handleDownload(javafx.event.ActionEvent actionEvent) {
         // You can leave it empty if you don't need it yet
         System.out.println("Download clicked");

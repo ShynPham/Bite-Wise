923a9fbdd2eaeba66acbd652a9f7e86d6e4897ec
AsianBMO <nguyen.angel@aol.com>
2025-11-11 12:06:39 -0600

    I deleted     requires edu.utsa.cs.group.bitewise;

--- BiteWise/src/main/java/module-info.java
+++ BiteWise/src/main/java/module-info.java
@@ -5,7 +5,6 @@ module edu.utsa.cs.group.bitewise {
     requires java.desktop;
     requires javafx.graphics;
     requires java.prefs;
-    requires edu.utsa.cs.group.bitewise;
 
 
     opens detectionApp to javafx.fxml, javafx.graphics;
